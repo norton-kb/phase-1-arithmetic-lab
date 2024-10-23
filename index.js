35dd56aae181a57b4a7ef017e5e4dce6d31ff726
norton-kb <biinorton080@gmail.com>
2024-10-23 09:23:37 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+const random = Math.floor(Math.random() * 100) + 1;
+let num3 = 25;
+let num4 = 7;
+const mod = num3 % num4;
+const max = Math.max(8, 19, 15, 20);
